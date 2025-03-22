d1627453c4d569ddc3a5b43f9139a9f84682a466
Joris Depoortere <joris.depoortere@outlook.com>
2025-03-22 09:55:43 +0100

    Laptop work 21/03

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -19,14 +19,7 @@ class LSTM(nn.Module):
         """
         super().__init__()
 
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.output_size = output_size
-        self.dropout = dropout
-
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
-
         self.linear = nn.Linear(in_features=hidden_size, out_features=output_size)
 
     def forward(self, input):
